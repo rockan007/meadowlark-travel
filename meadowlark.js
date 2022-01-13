93b79217a250370f7ee80102ebcdd86d26d8e89d
rockan007 <rockan007@gmail.com>
2022-01-13 13:55:18 +0000

    handlebars,static files,dynamic contents

--- meadowlark.js
+++ meadowlark.js
@@ -1,28 +1,41 @@
 const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
 const app = express()
+app.engine('handlebars',expressHandlebars.engine({
+    defaultLayout:'main'
+}))
+app.set('view engine','handlebars')
+
 const port = process.env.PORT||3000
 
-app.get('/',(req,res)=>{
-    res.type('text/plain')
-    res.send('Meadowlark Travel')
-})
 
-res.get('/about',(re,res)=>{
-    res.type('text/plain')
-    res.send('About Meadowlark Travel')
+const fortunes =[
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Don't fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible,keep it simple."
+]
+
+app.use(express.static(__dirname+'/public'))
+
+app.get('/',(req,res)=>res.render('home'))
+
+app.get('/about',(req,res)=>{
+    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.render('about',{fortune: randomFortune})
 })
 // 定制404页
 app.use((req,res)=>{
-    res.type('text/plain')
     res.status(404)
-    res.send('404 - Not Found')
+    res.render('404')
 })
 // 定制500页
 app.use((err,req,res,next)=>{
     console.error(err.message)
-    res.type('text/plain')
     res.status(500)
-    res.send('500 - Server Error')
+    res.render('500')
 })
 
 app.listen(port,()=>console.log(`Express started on http://localhost:${port}; `+'press Ctrl+C to terminate.'))
