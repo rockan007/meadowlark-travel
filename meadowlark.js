871b03e63f8fe8617eebd1516c76fa2dcac3515c
rockan007 <rockan007@gmail.com>
2022-01-14 13:28:49 +0000

    Moved 'fortune cookie' into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const fortune = require('./lib/fortune')
 
 const app = express()
 app.engine('handlebars',expressHandlebars.engine({
@@ -10,21 +11,15 @@ app.set('view engine','handlebars')
 const port = process.env.PORT||3000
 
 
-const fortunes =[
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Don't fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible,keep it simple."
-]
+
 
 app.use(express.static(__dirname+'/public'))
 
 app.get('/',(req,res)=>res.render('home'))
 
 app.get('/about',(req,res)=>{
-    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-    res.render('about',{fortune: randomFortune})
+   
+    res.render('about',{fortune: fortune.getFortune()})
 })
 // 定制404页
 app.use((req,res)=>{
