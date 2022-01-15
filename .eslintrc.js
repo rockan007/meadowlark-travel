be0cada6b96bdeb4fbc088787cf0b67db4281762
rockan007 <rockan007@gmail.com>
2022-01-15 13:51:08 +0000

    unit test,integration test and CI

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "commonjs": true,
+        "es2021": true,
+        "jest":true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 13
+    },
+    "rules": {
+        "no-console":"off"
+    }
+};
---
+++ integration-tests/basic-navigation.test.js
@@ -0,0 +1,31 @@
+const portfinder = require('portfinder')
+const puppeteer = require('puppeteer')
+
+const app = require('../meadowlark.js')
+
+jest.setTimeout(50000);
+
+let server = null
+let port = null
+beforeEach(async ()=>{
+    port = await portfinder.getPortPromise()
+    server = app.listen(port)
+})
+
+afterEach(()=>{
+    server.close()
+})
+
+test('home page links to about page',async ()=>{
+    const browser = await puppeteer.launch({
+        executablePath:'/snap/bin/chromium'
+    })
+    const page = await browser.newPage()
+    await page.goto(`http://localhost:${port}`)
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('[data-test-id="about"]')
+    ])
+    expect(page.url()).toBe(`http://localhost:${port}/about`)
+    await browser.close()
+})
