1bf88e4d3cd49d8ed08fdf4ae3ab5bf049127bc9
rockan007 <rockan007@gmail.com>
2022-01-17 13:54:22 +0000

    视图模板 handlebars--pug,handlebars,sections,partial

--- meadowlark.js
+++ meadowlark.js
@@ -1,20 +1,37 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const handlers = require('./lib/handlers')
+const weatherMiddlware = require('./lib/middleware/weather')
 
 const app = express()
 app.engine('handlebars',expressHandlebars.engine({
-    defaultLayout:'main'
+    defaultLayout:'main',
+    helpers:{
+        section:function(name,options){
+            if(!this._sections){
+                this._sections={}
+            }
+            this._sections[name] = options.fn(this)
+            return null
+        }
+    }
 }))
+
+app.disable('x-powered-by')
+
 app.set('view engine','handlebars')
 
 const port = process.env.PORT||3000
 
 app.use(express.static(__dirname+'/public'))
 
+app.use(weatherMiddlware)
+
 app.get('/',handlers.home)
 
 app.get('/about',handlers.about)
+
+app.get('/section',handlers.section)
 // 定制404页
 app.use(handlers.notFound)
 // 定制500页
