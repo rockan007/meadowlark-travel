1e4edd2c05fea73066e928b51161758a15bc341e
rockan007 <rockan007@gmail.com>
2022-01-22 13:51:12 +0000

    fetchAPI与上传文件

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const expressHandlebars = require('express-handlebars')
+const multiparty = require('multiparty')
 const handlers = require('./lib/handlers')
 const weatherMiddlware = require('./lib/middleware/weather')
 
@@ -27,6 +28,7 @@ const port = process.env.PORT||3000
 app.use(express.static(__dirname+'/public'))
 
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
 app.use(weatherMiddlware)
 
@@ -35,10 +37,25 @@ app.get('/',handlers.home)
 app.get('/about',handlers.about)
 
 app.get('/section',handlers.section)
-
+//fetch
+app.get('/newsletter',handlers.newsletter)
+app.post('/api/newsletter-signup',handlers.api.newsletterSignup)
 app.get('/newsletter-signup',handlers.newsletterSignup)
 app.post('/newsletter-signup/process',handlers.newsletterSignupProcess)
 app.get('/newsletter-signup/thank-you',handlers.newsletterSignupThankYou)
+
+app.get('/contest/vacation-photo',handlers.vacationPhoto)
+// 上传文件
+app.post('/contest/vacation-photo/:year/:month',(req,res)=>{
+    const form = new multiparty.Form()
+    form.parse(req,(err,fields,files)=>{
+        if(err) return res.status(500).send({error:err.message})
+        handlers.vacationPhotoContestProccess(req,res,fields,files)
+    })
+})
+app.get('/contest/vacation-photo-contest',handlers.vacationPhotoContest)
+app.post('/api/vacation-photo-contest/:year/:month',handlers.api.newsletterSignup)
+app.get('/contest/vacation-photo-thank-you',handlers.vacationPhotoThankYou)
 // 定制404页
 app.use(handlers.notFound)
 // 定制500页
